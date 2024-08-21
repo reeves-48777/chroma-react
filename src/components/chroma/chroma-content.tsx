248d36c6e347bfc9339a48ed4986c2a1c1ccff61
Thomas Durand <durandthomas@protonmail.com>
2024-08-21 17:59:49 +0200

    style - improved contrast in the chroma content
    - lowered the background opacity
    - made the text color darker for the 'no palette for now' span

--- src/components/chroma/chroma-content.tsx
+++ src/components/chroma/chroma-content.tsx
@@ -9,7 +9,7 @@ export default function ChromaContent() {
   return (
     <div
       className={cn(
-        'relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 items-center justify-center',
+        'relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/30 px-6 pt-12 pb-6 gap-12 items-center justify-center',
         isContextVisible
           ? 'lg:col-span-2 md:col-span-1'
           : 'lg:col-span-3 md:col-span-2'
@@ -19,10 +19,10 @@ export default function ChromaContent() {
         variant='outline'
         className='absolute right-3 top-3'
       >
-        Palette
+        Palette editor
       </Badge>
       <Palette />
-      <div className='relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring mt-4 p-4 flex items-center'>
+      <div className='relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 p-4 flex items-center'>
         <PaletteCommands className='w-full' />
       </div>
     </div>
