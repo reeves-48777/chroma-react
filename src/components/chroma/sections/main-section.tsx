41df81485d7996944503a3dfe810e6bf7460d98a
Thomas Durand <durandthomas@protonmail.com>
2024-08-21 15:48:52 +0200

    refactor - moved main section content to it's own component

---
+++ src/components/chroma/sections/main-section.tsx
@@ -0,0 +1,25 @@
+import ChromaContent from '@/components/chroma/chroma-content';
+import ChromaContext from '@/components/chroma/chroma-context';
+import { Spinner } from '@/components/ui/spinner';
+import useUIStore from '@/stores/ui';
+import { Suspense } from 'react';
+
+export default function MainSection() {
+  const Menu = useUIStore((state) => state.menu);
+  return (
+    <div className='w-full h-full gap-4 flex relative'>
+      <ChromaContext>
+        <Suspense
+          fallback={
+            <Spinner className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2' />
+          }
+        >
+          {<Menu />}
+        </Suspense>
+      </ChromaContext>
+      <div className='flex-1'>
+        <ChromaContent />
+      </div>
+    </div>
+  );
+}
