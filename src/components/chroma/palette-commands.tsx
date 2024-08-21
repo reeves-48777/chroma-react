8b9579a031f4734ceb1f88f02767f8b9390037bc
Thomas Durand <durandthomas@protonmail.com>
2024-08-21 16:27:06 +0200

    refactor/fix - palette commands
    -download palette button is now disabled if there is no palette
    
    - using the toggleOrientation function from UIStore

--- src/components/chroma/palette-commands.tsx
+++ src/components/chroma/palette-commands.tsx
@@ -1,13 +1,15 @@
-import { Button } from '../ui/button';
+import { Button } from '@/components/ui/button';
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip';
+// import { useToast } from '@/components/ui/use-toast';
 import { usePalette } from '@/hooks/palette';
 import useUIStore from '@/stores/ui';
 import { RefreshCw, Download } from 'lucide-react';
+// import { TriangleAlert } from 'lucide-react';
 import { MouseEventHandler } from 'react';
 
 type PaletteCommandsProps = {
@@ -15,9 +17,12 @@ type PaletteCommandsProps = {
 };
 
 const PaletteCommands = ({ className = '' }: PaletteCommandsProps) => {
-  const orientation = useUIStore((state) => state.paletteOrientation);
-  const setOrientation = useUIStore((state) => state.setPaletteOrientation);
-  const { capturePaletteAsImage } = usePalette();
+  // const { toast } = useToast();
+  const toggleOrientation = useUIStore(
+    (state) => state.togglePaletteOrientation
+  );
+
+  const { isPaletteEmpty, capturePaletteAsImage } = usePalette();
 
   const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.preventDefault();
@@ -25,13 +30,24 @@ const PaletteCommands = ({ className = '' }: PaletteCommandsProps) => {
 
   const handleRotateClick: MouseEventHandler<HTMLButtonElement> = (event) => {
     handleClick(event);
-    orientation === 'vertical'
-      ? setOrientation('horizontal')
-      : setOrientation('vertical');
+    toggleOrientation();
   };
 
   const handleDownloadClick: MouseEventHandler<HTMLButtonElement> = (event) => {
     handleClick(event);
+    // if (isPaletteEmpty) {
+    //   toast({
+    //     title: 'Something went wrong :c',
+    //     description: (
+    //       <div className='flex items-center gap-4'>
+    //         You cannot download an empty palette <TriangleAlert />
+    //       </div>
+    //     ),
+    //     className: 'text-red-500',
+    //   });
+    // } else {
+    //   capturePaletteAsImage('palette-container');
+    // }
     capturePaletteAsImage('palette-container');
   };
 
@@ -57,6 +73,7 @@ const PaletteCommands = ({ className = '' }: PaletteCommandsProps) => {
           <TooltipTrigger asChild>
             <Button
               className='rounded-lg gap-3'
+              disabled={isPaletteEmpty}
               onClick={handleDownloadClick}
               variant='secondary'
               size='sm'
