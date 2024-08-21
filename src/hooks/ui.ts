47d0217f4bce60a4f14603ade44082373adf98ad
Thomas Durand <durandthomas@protonmail.com>
2024-08-21 15:52:03 +0200

    feat - added a new ui hook to get whether the device is mobile or desktop

---
+++ src/hooks/ui.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState, useRef } from 'react';
+
+type DeviceType = 'mobile' | 'desktop';
+
+export const useUI = () => {
+  const [isMobileDevice, setIsMobileDevice] = useState(
+    window.matchMedia('(max-width: 767px)').matches
+  );
+
+  const deviceType = useRef<DeviceType>(isMobileDevice ? 'mobile' : 'desktop');
+
+  useEffect(() => {
+    const handleResize = () => {
+      console.log('yippeeee');
+      setIsMobileDevice(window.matchMedia('(max-width: 767px)').matches);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return {
+    isMobileDevice,
+    deviceType,
+  };
+};
