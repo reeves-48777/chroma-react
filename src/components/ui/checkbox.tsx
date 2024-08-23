be8071a5cb7ed9a1ab3a24841d24fecefe3bc027
Thomas Durand <durandthomas@protonmail.com>
2024-08-23 12:15:31 +0200

    chore - shadcn checkbox back to default

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -3,35 +3,22 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'lucide-react';
 import * as React from 'react';
 
-type CheckboxProps = React.ComponentPropsWithoutRef<
-  typeof CheckboxPrimitive.Root
-> & {
-  color?: string;
-};
-
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
-  CheckboxProps
->(({ className, color, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'peer shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-      color
-        ? 'size-6 data-[state=checked]:outline data-[state=checked]:outline-2 data-[state=checked]:outline-primary data-[state=checked]:outline-offset-2 data-[state=checked]:text-transparent'
-        : ' size-4 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+      'peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
       className
     )}
-    style={{
-      background: color ? color : 'transparent',
-      borderColor: props.checked ? 'currentColor' : color,
-    }}
     {...props}
   >
     <CheckboxPrimitive.Indicator
       className={cn('flex items-center justify-center text-current')}
     >
-      <Check className={color ? 'size-6' : 'size-4'} />
+      <Check className='h-4 w-4' />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
